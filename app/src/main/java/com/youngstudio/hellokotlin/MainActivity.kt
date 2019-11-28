227fbd6f709f0d9188be2e54ef045df60aded7bc
origin <ant92111@gamil.com>
2019-11-28 15:17:46 +0900

    1

---
+++ app/src/main/java/com/youngstudio/hellokotlin/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.youngstudio.hellokotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun clickBtn(view: View) {
+        //XML에서 지정한 id를 곧바로 변수명으로 사용
+        tv.setText("Nice to meet you. Kotlin!!")
+    }
+
+    override fun onResume(){
+        super.onResume()
+        Toast.makeText(this,"onResume",Toast.LENGTH_SHORT).show()
+    }
+
+
+}
---
+++ app/src/main/java/com/youngstudio/hellokotlin/MykotlinClass.kt
@@ -0,0 +1,15 @@
+package com.youngstudio.hellokotlin
+
+class MykotlinClass {
+    //property
+    var a=10
+    val b=20
+
+    //method
+    fun show(){
+        println(a)
+        println(b)
+        println()
+    }
+
+}
